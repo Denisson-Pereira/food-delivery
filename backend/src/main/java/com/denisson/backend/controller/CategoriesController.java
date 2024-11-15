c41194ece752d39a3c8260b52738b1d895f17840
Denisson Pereira Santos <denisson.pereira753@gmail.com>
2024-11-15 00:16:03 -0300

    Update CategoriesController.java

--- backend/src/main/java/com/denisson/backend/controller/CategoriesController.java
+++ backend/src/main/java/com/denisson/backend/controller/CategoriesController.java
@@ -7,9 +7,15 @@ import com.denisson.backend.model.Category;
 import com.denisson.backend.repository.CategoryRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 @RestController
@@ -23,6 +29,22 @@ public class CategoriesController {
     public List<Category> getAll() {
         return repository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable Long id) {
+        try {
+            if (repository.existsById(id)) {
+                return ResponseEntity.ok(repository.findById(id));
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(String.format("Id %s not found!", id));
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+
+
+    }
+    
     
     
     
