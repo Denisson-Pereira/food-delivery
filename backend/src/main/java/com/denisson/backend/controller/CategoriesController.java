f0dc8d4885b5708c9ffe77bf49cd7f40118c0e93
Denisson Pereira Santos <denisson.pereira753@gmail.com>
2024-11-15 18:26:04 -0300

    feat: :sparkles: Update controller

--- backend/src/main/java/com/denisson/backend/controller/CategoriesController.java
+++ backend/src/main/java/com/denisson/backend/controller/CategoriesController.java
@@ -7,14 +7,16 @@ import com.denisson.backend.model.Category;
 import com.denisson.backend.repository.CategoryRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -41,9 +43,39 @@ public class CategoriesController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
-
-
     }
+
+    @PostMapping("")
+    public ResponseEntity<?> create(@RequestBody Category category) {
+        try {
+            if (category == null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Category cannot be null!");
+            }
+            
+            Category savedCategory = repository.save(category);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred: " + e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        try {
+            if (!repository.existsById(id)) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body(String.format("Id %s not found!", id));
+            }
+            repository.deleteById(id);
+            return ResponseEntity.ok("Category deleted!");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An internal error occurred. Please try again later.");
+        }
+    }
+    
+    
+    
     
     
     
