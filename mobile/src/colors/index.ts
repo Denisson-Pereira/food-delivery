18db23860dd3ecc46f2661c8d2a5128bc4b1a3ae
Denisson Pereira Santos <denisson.pereira753@gmail.com>
2024-11-20 20:58:56 -0300

    feat: :sparkles: Update products

--- mobile/src/colors/index.ts
+++ mobile/src/colors/index.ts
@@ -2,5 +2,6 @@ export const colors = {
     grey: '#ABABAB',
     white: '#FFFFFF',
     orange: '#FF9431',
-    black: '#000'
+    black: '#000',
+    yellow: '#FFD600'
 }
--- mobile/src/views/HomeView.tsx
+++ mobile/src/views/HomeView.tsx
@@ -1,4 +1,4 @@
-import { Fontisto } from "@expo/vector-icons"
+import { AntDesign, Entypo, Fontisto } from "@expo/vector-icons"
 import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native"
 import { colors } from "../colors"
 import { FindBar, Header, Title } from "../components"
@@ -47,9 +47,35 @@ export const HomeView = () => {
                     ))}
                 </ScrollView>
             </View>
-            <View>
+            <View style={styles.grid}>
                 {filteredProducts.map((item) => (
-                    <Text key={item.id}>{item.name}</Text>
+                    <TouchableOpacity 
+                        key={item.id}
+                        style={styles.container}    
+                    >
+                        <View style={styles.star}>
+                            <Entypo name="star" size={20} color={colors.yellow} />
+                            <Text>{item.evaluation}</Text>
+                        </View>
+                        <View style={styles.image}>
+                            <Image
+                                source={{ uri: item.image }}
+                                style={{ width: 100, height: 100, resizeMode: 'contain' }}
+                            />
+                        </View>
+                        <Text style={styles.name}>{item.name}</Text>
+                        <Text style={styles.description}>{item.description}</Text>
+                        <View style={styles.containerPrice}>
+                            <View style={styles.price}>
+                                <Text style={styles.txtprice1}>$ </Text>
+                                <Text style={styles.txtprice2}>{item.price}.</Text>
+                                <Text style={styles.txtprice3}>00</Text>
+                            </View>
+                            <TouchableOpacity style={styles.plus}>
+                                <AntDesign name="plus" size={20} color={colors.white} />
+                            </TouchableOpacity>
+                        </View>
+                    </TouchableOpacity>
                 ))}
             </View>
         </ScrollView>
@@ -106,5 +132,74 @@ const styles = StyleSheet.create({
         fontSize: 20,
         color: colors.black,
         fontWeight: '700'
+    },
+    star: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 5
+    },
+    container: {
+        width: "48%",
+        padding: 10,
+        backgroundColor: colors.white,
+        borderRadius: 10,
+        shadowColor: '#8f8f8f44',
+        shadowOffset: { width: 10, height: 5 },
+        shadowOpacity: 0.55,
+        shadowRadius: 10,
+        elevation: 10,
+        gap: 5
+    },
+    grid: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+        padding: 10,
+        gap: 10,
+        marginTop: 10,
+        marginBottom: 150
+    },
+    image: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    description: {
+        fontSize: 13
+    },
+    name: {
+        fontSize: 20,
+        fontWeight: '600',
+    },
+    plus: {
+        backgroundColor: colors.orange,
+        width: 30,
+        height: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 50
+    },
+    containerPrice: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+        
+    },
+    price: {
+        flexDirection: 'row',
+        alignItems: 'flex-end'
+    },
+    txtprice1: {
+        fontSize: 18,
+        fontWeight: '700',
+        color: colors.orange
+    },
+    txtprice2: {
+        fontSize: 18,
+        fontWeight: '700',
+        color: colors.orange
+    },
+    txtprice3: {
+        fontWeight: '500',
+        color: colors.orange
     },
 })
