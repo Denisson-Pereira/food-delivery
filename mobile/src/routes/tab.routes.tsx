a47f54a719e62e16237bb5f6baf95b71f7c90144
Denisson Pereira Santos <denisson.pereira753@gmail.com>
2024-11-20 08:44:15 -0300

    feat: :sparkles: Update tabbar

--- mobile/src/routes/tab.routes.tsx
+++ mobile/src/routes/tab.routes.tsx
@@ -1,50 +1,115 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-import { CartView, FavoritiesView, HomeView } from "../views";
-import { Feather, SimpleLineIcons, FontAwesome } from '@expo/vector-icons';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { CartView, FavoritiesView, HomeView } from "../views"; 
+import { Feather, SimpleLineIcons, FontAwesome } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 export const TabRoutes = () => {
     return (
         <Tab.Navigator
-            screenOptions={{
-                headerShown: true,
-                headerStyle: {
-                    backgroundColor: '#00A6EB',
-                },
-                headerTintColor: '#fff',
+            screenOptions={({ route }) => ({
+                headerShown: false,
+                tabBarShowLabel: true,
+                tabBarActiveTintColor: "#FE724C",
+                tabBarInactiveTintColor: "#D3D1D8", 
                 tabBarStyle: {
-                    alignItems: 'center',
+                    height: 110,
                     justifyContent: 'center',
-                    height: 90,
-                    backgroundColor: '#ffffff',
+                    borderTopLeftRadius: 30, 
+                    borderTopRightRadius: 30, 
+                    overflow: "hidden",
                 },
-                tabBarActiveTintColor: '#00A6EB',
-            }}
+                tabBarIconStyle: {
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginTop: 20
+                }
+            })}
         >
             <Tab.Screen
-                name='home'
+                name="homeView"
                 component={HomeView}
                 options={{
-                    tabBarIcon: ({ color, size }) => <Feather name='home' color={color} size={size} />
+                    tabBarIcon: ({ color, size, focused }) => (
+                        <View style={{ alignItems: "center" }}>
+                            {focused && <View style={styles.activeBar} />}
+                            <Feather name="home" color={color} size={size} />
+                        </View>
+                    ),
+                    tabBarLabel: ({ focused }) => (
+                        <Text
+                            style={{
+                                fontSize: 12,
+                                fontWeight: focused ? "bold" : "normal",
+                                color: focused ? "#FE724C" : "#D3D1D8",
+                            }}
+                        >
+                            Home
+                        </Text>
+                    ),
                 }}
             />
 
             <Tab.Screen
-                name='cart'
+                name="cartView"
                 component={CartView}
                 options={{
-                    tabBarIcon: ({ color, size }) => <SimpleLineIcons name='list' color={color} size={size} />
+                    tabBarIcon: ({ color, size, focused }) => (
+                        <View style={{ alignItems: "center" }}>
+                            {focused && <View style={styles.activeBar} />} 
+                            <SimpleLineIcons name="list" color={color} size={size} />
+                        </View>
+                    ),
+                    tabBarLabel: ({ focused }) => (
+                        <Text
+                            style={{
+                                fontSize: 12,
+                                fontWeight: focused ? "bold" : "normal",
+                                color: focused ? "#FE724C" : "#D3D1D8",
+                            }}
+                        >
+                            Cart
+                        </Text>
+                    ),
                 }}
             />
 
             <Tab.Screen
-                name='favorities'
+                name="favoritiesView"
                 component={FavoritiesView}
                 options={{
-                    tabBarIcon: ({ color, size }) => <FontAwesome name='heart' color={color} size={size} />
+                    tabBarIcon: ({ color, size, focused }) => (
+                        <View style={{ alignItems: "center" }}>
+                            {focused && <View style={styles.activeBar} />} 
+                            <FontAwesome name="heart" color={color} size={size} />
+                        </View>
+                    ),
+                    tabBarLabel: ({ focused }) => (
+                        <Text
+                            style={{
+                                fontSize: 12,
+                                fontWeight: focused ? "bold" : "normal",
+                                color: focused ? "#FE724C" : "#D3D1D8",
+                            }}
+                        >
+                            Favorites
+                        </Text>
+                    ),
                 }}
             />
         </Tab.Navigator>
-    )
-}
+    );
+};
+
+const styles = StyleSheet.create({
+    activeBar: {
+        height: 8,
+        width: 40,
+        backgroundColor: "#FE724C", 
+        borderBottomEndRadius: 20,
+        borderBottomStartRadius: 20,
+        position: "absolute",
+        top: -25,
+    },
+});
